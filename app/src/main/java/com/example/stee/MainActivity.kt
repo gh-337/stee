7a6f7a4103ff3d83796b06fde20e3b33cb66b7a2
gh-337 <neyro337@gmail.com>
2023-04-18 01:36:52 +0300

    every day! sav

--- app/src/main/java/com/example/stee/MainActivity.kt
+++ app/src/main/java/com/example/stee/MainActivity.kt
@@ -4,20 +4,57 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.example.stee.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val sharedPref = getSharedPreferences("myPref", MODE_PRIVATE)
+        val editor =sharedPref.edit()
+
+        binding.apply {
+            btnSave.setOnClickListener {
+                val userName = edtUsername.text.toString() //-значеня
+                val email = edtEmail.text.toString()
+
+                editor.apply {
+                    putString("user_name",userName) //dbljlbvj
+                    putString("email",email)
+                    apply()
+                }
+            }
+
+            btnLoad.setOnClickListener {
+
+                val userName = sharedPref.getString("user_name",null)
+                val email = sharedPref.getString("email",null)
+
+                tvUsername.text = userName
+                tvEmail.text=email
+            }
+        }
+
+    }
+}
+
+
+
+/*class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    fun oneClikAc2 (viaw : View){
-        val intent = Intent(this , ac_2 ::class.java)
+    fun oneClikLogin (viaw : View){
+        val intent = Intent(this , Login ::class.java)
         startActivity(intent)
     }
 
-    fun oneClikAc3 (viaw : View){
-        val intent = Intent(this , ac_3 ::class.java)
+    fun oneClikRegister (viaw : View){
+        val intent = Intent(this , Register ::class.java)
         startActivity(intent)
     }
-}
+}*/
